6e39b9e63130625ccbef2c01a146aad88b55ccb9
Yoon Han Sol <yhs1514@hanmail.net>
2020-11-25 17:01:08 +0900

    ~ about contents, and about's css

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 export default function Nav(props) {
     return(
         <ul className="app-header-nav-ul">
-            <li className="app-header-nav-ul-li button-rubber"><Link to="/about">이력</Link></li>
-            <li className="app-header-nav-ul-li button-rubber"><Link to="/diary">일기장</Link></li>
+            <li className="app-header-nav-ul-li"><Link to="/about" className="button-rubber">Portfolio</Link></li>
+            <li className="app-header-nav-ul-li"><Link to="/diary" className="button-rubber">Diary</Link></li>
         </ul>
     )
 }
--- src/main/pages/About.js
+++ src/main/pages/About.js
@@ -4,7 +4,9 @@ import githubIcon from '../../assets/images/github.png';
 import instagramIcon from '../../assets/images/instagram.png';
 
 export default function About() {
-    const [curTab, setCurTab] = useState(0);
+    const [curTab, setCurTab] = useState(-1);
+    const [isOffsets, setIsOffsets] = useState(false);
+    const [myOffsets, setMyOffsets] = useState([]);
 
     function mainTabClicked(event) {
         const curTabIdx = event.target.dataset.idx;
@@ -24,11 +26,75 @@ export default function About() {
         openInNewTab("https://www.instagram.com/hanso_l_/");
     }
 
+    function addMoreOffset(idx, offset) {
+        setMyOffsets(prevItems => [...prevItems, {
+            idx, offset
+        }])
+    }
+
+    function getOffSetTop() {
+        const elem = document.getElementById('about-tab-container');
+        for (let i=0; i<elem.children.length; i++) {
+            addMoreOffset(i, elem.children[i].offsetTop);
+        }
+        setIsOffsets(true);
+    }
+
+    function scrollEvent(event) {
+        const curScroll = window.scrollY + 5;
+        const boxElem = document.getElementById('about-tab-box');
+        let curTabIdx = curTab;
+        if (curScroll < myOffsets[0].offset) {
+            curTabIdx = -1;
+            setCurTab(curTabIdx);
+            if (boxElem.className.includes('about-tab-appear')) {
+                boxElem.className = boxElem.className.replace('about-tab-appear', 'about-tab-disappear');
+            }
+        } else if (curScroll >= myOffsets[0].offset && curScroll < myOffsets[1].offset) {
+            curTabIdx = 0;
+            setCurTab(curTabIdx);
+            if (boxElem.className.includes('about-tab-init')) {
+                boxElem.className = boxElem.className.replace('about-tab-init', 'about-tab-appear');
+            } else if (boxElem.className.includes('about-tab-disappear')) {
+                boxElem.className = boxElem.className.replace('about-tab-disappear', 'about-tab-appear');
+            }
+        } else if (curScroll >= myOffsets[1].offset && curScroll < myOffsets[2].offset) {
+            curTabIdx = 1;
+            setCurTab(curTabIdx);
+        } else if (curScroll >= myOffsets[2].offset && curScroll < myOffsets[3].offset) {
+            curTabIdx = 2;
+            setCurTab(curTabIdx);
+        } else if (curScroll >= myOffsets[3].offset && curScroll < myOffsets[4].offset) {
+            curTabIdx = 3;
+            setCurTab(curTabIdx);
+        } else if (curScroll >= myOffsets[4].offset && curScroll < myOffsets[5].offset) {
+            curTabIdx = 4;
+            setCurTab(curTabIdx);
+        } else if (curScroll >= myOffsets[5].offset) {
+            curTabIdx = 5;
+            setCurTab(curTabIdx);
+        }
+    }
+
+    useEffect(() => {
+        getOffSetTop();
+    }, []);
+
+    useEffect(() => {
+        if (isOffsets === true) {
+            window.addEventListener('scroll', scrollEvent);
+            return () => {
+                window.removeEventListener('scroll', scrollEvent);
+        }
+        }
+    }, [isOffsets])
+
     useEffect(() => {
         const parent = document.getElementById('app-main-about-left-nav-ul-0');
         for (let i = 0; i < parent.children.length; i++) {
             parent.children[i].children[1].className = parent.children[i].children[1].className.replace(' tab-active', '')
         }
+        if (curTab < 0) return;
         parent.children[curTab].children[1].className += ' tab-active'
     }, [curTab])
 
@@ -49,7 +115,10 @@ export default function About() {
                                 About
                             </span>
                             <ul className="app-main-about-left-nav-ul-li-ul">
-                                <li><a href="#about-tab-00">intro</a></li>
+                                <li className="app-main-about-left-nav-ul-li-ul-li"><a href="#about-tab-00">intro</a></li>
+                                <li><a href="#about-tab-00-00">Name</a></li>
+                                <li><a href="#about-tab-00-01">Description</a></li>
+                                <li><a href="#about-tab-00-02">href</a></li>
                             </ul>
                         </li>
                         <li>
@@ -62,6 +131,8 @@ export default function About() {
                             </span>
                             <ul className="app-main-about-left-nav-ul-li-ul">
                                 <li><a href="#about-tab-01">intro</a></li>
+                                <li><a href="#about-tab-01-00">University</a></li>
+                                <li><a href="#about-tab-01-01">High school</a></li>
                             </ul>
                         </li>
                         <li>
@@ -74,6 +145,9 @@ export default function About() {
                             </span>
                             <ul className="app-main-about-left-nav-ul-li-ul">
                                 <li><a href="#about-tab-02">intro</a></li>
+                                <li><a href="#about-tab-02-00">major</a></li>
+                                <li><a href="#about-tab-02-01">minor</a></li>
+                                <li><a href="#about-tab-02-02">etc</a></li>
                             </ul>
                         </li>
                         <li>
@@ -86,6 +160,8 @@ export default function About() {
                             </span>
                             <ul className="app-main-about-left-nav-ul-li-ul">
                                 <li><a href="#about-tab-03">intro</a></li>
+                                <li><a href="#about-tab-03-00">school</a></li>
+                                <li><a href="#about-tab-03-01">society</a></li>
                             </ul>
                         </li>
                         <li>
@@ -113,28 +189,40 @@ export default function About() {
                             </ul>
                         </li>
                     </ul>
+                    <div className="space-vertical" />
+                    <div id="about-tab-box" className="app-main-about-left-nav-div about-tab-init">
+                        <span>Yoon Han Sol</span>
+                    </div>
                 </nav>
             </div>
 
-            <div className="app-main-about-right">
+            <div className="app-main-about-right" id="about-tab-container">
                 <div
                     id="about-tab-00"
                     className="app-main-about-right-div"
                 >
-                    <h2 class="app-main-about-right-div-title">
+                    <h2 className="app-main-about-right-div-title">
                         About
                     </h2>
-                    <p>
+                    <div id="about-tab-00-00">
                         <span style={{fontSize: '30px'}}>HANSOL</span>
                         <span> </span>
                         <span style={{fontSize: '20px'}}>YOON</span>
-                    </p>
-                    <p style={{display: 'flex', flexDirection: 'column', fontSize: "14px"}}>
+                    </div>
+                    <div className="space-vertical" />
+                    <div
+                        style={{display: 'flex', flexDirection: 'column', fontSize: "14px"}}
+                        id="about-tab-00-01"
+                    >
                         <span>주소 : 경기도 용인시 처인구 금령로173번길 5</span>
                         <span>번호 : +82-10-9099-8225</span>
                         <span>Email : yhs3434@gmail.com</span>
-                    </p>
-                    <p style={{display: 'flex', alignItems: 'center'}}>
+                    </div>
+                    <div className="space-vertical" />
+                    <div
+                        style={{display: 'flex', alignItems: 'center'}}
+                        id="about-tab-00-02"
+                    >
                         <img
                             src={githubIcon}
                             width='30px'
@@ -147,7 +235,7 @@ export default function About() {
                             className="app-main-about-right-div-p-img instagram-icon"
                             onClick={instagramIconClicked}
                         />
-                    </p>
+                    </div>
                 </div>
 
                 <div
@@ -157,9 +245,12 @@ export default function About() {
                     <h2 className="app-main-about-right-div-title">
                         Education
                     </h2>
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <div
+                        style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}
+                        id="about-tab-01-00"    
+                    >
                         <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <span>University Of Dankook</span>
+                            <h3>University Of Dankook</h3>
                             <span>Bachelor Of Computer Science</span>
                             <span>Software Engineering</span>
                         </div>
@@ -167,9 +258,13 @@ export default function About() {
                             <span>2013.03 ~ 2020.08</span>
                         </div>
                     </div>
-                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                    <div className="space-vertical" style={{height: '40px'}}/>
+                    <div
+                        style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}
+                        id="about-tab-01-01"    
+                    >
                         <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <span>Taesung High School</span>
+                            <h3>Taesung High School</h3>
                             <span>Math / Science</span>
                         </div>
                         <div style={{display: 'flex', flexDirection: 'column'}}>
@@ -185,7 +280,45 @@ export default function About() {
                     <h2 className="app-main-about-right-div-title">
                         Skills
                     </h2>
-                    section 02
+                    <div id="about-tab-02-00">
+                        <h3>Major</h3>
+                        <div>
+                            <h4>front end</h4>
+                            PWA, HTML5, CSS3, JAVASCRIPT (es6 이전, 이후 모두 가능), React, Vue
+                        </div>
+                        <div>
+                            <h4>back end</h4>
+                            Websocket (ws protocol)
+                        </div>
+                    </div>
+                    <div className="space-vertical" style={{height: '100px'}}/>
+                    <div id="about-tab-02-01">
+                        <h3>Minor</h3>
+                        <div>
+                            <h4>front end</h4>
+                            Android (Java), jQuery
+                        </div>
+                        <div>
+                            <h4>back end</h4>
+                            Django, Flask, Express, Koa, PHP
+                        </div>
+                    </div>
+                    <div className="space-vertical" style={{height: '100px'}}/>
+                    <div id="about-tab-02-02">
+                        <h3>etc</h3>
+                        <div>
+                            <h4>Database</h4>
+                            MySQL, mongo db, indexedDB
+                        </div>
+                        <div>
+                            <h4>Language</h4>
+                            C, C++, JAVA, python, javascript, R
+                        </div>
+                        <div>
+                            <h4>Source code management</h4>
+                            git
+                        </div>
+                    </div>
                 </div>
 
                 <div
@@ -195,7 +328,28 @@ export default function About() {
                     <h2 className="app-main-about-right-div-title">
                         Projects
                     </h2>
-                    section 03
+                    <div id="about-tab-03-00">
+                        <h3>School</h3>
+                        <div>
+                            <h4>Suroom</h4>
+                            member: Yoon han sol(Leader), Park hong je, Lee dong gon
+                        </div>
+                        <div>
+                            <h4>Smart Career</h4>
+                            member: Yoon han sol(Leader), Lee yoo chan, Jung jin man, Han sang il
+                        </div>
+                        <div>
+                            <h4>Medical system for using blockchain</h4>
+                            member : Han su young(Leader), Kim je hyoung, Park si hwan, Yoon han sol
+                        </div>
+                    </div>
+                    <div className="space-vertical" />
+                    <div id="about-tab-03-01">
+                        <h3>Society</h3>
+                        <div>
+                            not yet...
+                        </div>
+                    </div>
                 </div>
 
                 <div
@@ -205,7 +359,13 @@ export default function About() {
                     <h2 className="app-main-about-right-div-title">
                         Interests
                     </h2>
-                    section 04
+                    <div>
+                        <h3>game</h3>
+                        <div>
+                            League of legends (Dia IV)
+                            Overwatch (Dia)
+                        </div>
+                    </div>
                 </div>
 
                 <div
