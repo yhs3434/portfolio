36ff87661ab0dd36f9ef250b716dc285d60950ca
Yoon Han Sol <yhs1514@hanmail.net>
2020-11-27 02:04:14 +0900

    Updates

---
+++ precache-manifest.507102a7c6b5e4653f767a640b41ad89.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8466fa66913285c4f1634ac0e622768",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "186fb06bdd19ef64a20b",
+    "url": "/portfolio/static/css/main.14cf8dd6.chunk.css"
+  },
+  {
+    "revision": "93d52d1028b08535d048",
+    "url": "/portfolio/static/js/2.fc064e12.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.fc064e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186fb06bdd19ef64a20b",
+    "url": "/portfolio/static/js/main.f12d7a7c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "25fb459fe19dee339cfef97170110dd3",
+    "url": "/portfolio/static/media/brain.25fb459f.jpg"
+  },
+  {
+    "revision": "1bbb79ec00c311c9e553c4d1e2605157",
+    "url": "/portfolio/static/media/chrome_reader_mode.1bbb79ec.svg"
+  },
+  {
+    "revision": "9252906eaa5b87c2114d91ccd5e79813",
+    "url": "/portfolio/static/media/face.9252906e.svg"
+  },
+  {
+    "revision": "e537824b0431f1f9a36696e84e64808f",
+    "url": "/portfolio/static/media/github.e537824b.png"
+  },
+  {
+    "revision": "774d88f0ace5b597ae9e9470f81aa87e",
+    "url": "/portfolio/static/media/instagram.774d88f0.png"
+  },
+  {
+    "revision": "953ce18afd7d1b959315189eed65aef1",
+    "url": "/portfolio/static/media/monochrome_photos.953ce18a.svg"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "/portfolio/static/media/search.db5d8122.svg"
+  }
+]);
